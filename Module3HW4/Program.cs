ad80c2ae7c5ab815c931cac81a8d5e4c3b888846
Pavel Kaznachei <pavel.kaznachei@gmail.com>
2021-12-16 12:01:12 +0200

    Complete task 1.

--- Module3HW4/Program.cs
+++ Module3HW4/Program.cs
@@ -6,7 +6,11 @@ namespace Module3HW4
     {
         public static void Main(string[] args)
         {
+            var task1 = new Task1();
+            Console.WriteLine("Task 1:\n");
+            task1.Run(5, 5);
             var task2 = new Task2();
+            Console.WriteLine("Task 2:\n");
             task2.Run();
         }
     }
--- Module3HW4/Task1.cs
+++ Module3HW4/Task1.cs
@@ -8,5 +8,42 @@ namespace Module3HW4
 {
     public class Task1
     {
+        public event Func<int, int, int> SumCompleted;
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int MethodsSum(int x, int y)
+        {
+            int sum = 0;
+            foreach (Func<int, int, int> function in SumCompleted.GetInvocationList())
+            {
+                sum += function(x, y);
+            }
+
+            Console.WriteLine(sum);
+            return sum;
+        }
+
+        public void TryCatch(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public void Run(int x, int y)
+        {
+            SumCompleted += Sum;
+            SumCompleted += Sum;
+            Console.WriteLine($"Sum({x}, {y})");
+            TryCatch(() => MethodsSum(x, y));
+        }
     }
 }
